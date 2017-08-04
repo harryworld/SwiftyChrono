fec2351e3f50511112b90b4e9451e6b7271a03a6
Harry Ng <harryworld@gmail.com>
2017-08-04 19:02:58 +0800

    Bug Fix on Invalid Month Name being detected by German Parser

--- Sources/Parsers/DE/DEMonthNameLittleEndianParser.swift
+++ Sources/Parsers/DE/DEMonthNameLittleEndianParser.swift
@@ -40,7 +40,9 @@ public class DEMonthNameLittleEndianParser: Parser {
         let (matchText, index) = matchTextAndIndex(from: text, andMatchResult: match)
         var result = ParsedResult(ref: ref, index: index, text: matchText)
         
-        let month = DE_MONTH_OFFSET[match.string(from: text, atRangeIndex: monthNameGroup).lowercased()]!
+        guard let month = DE_MONTH_OFFSET[match.string(from: text, atRangeIndex: monthNameGroup).lowercased()] else {
+            return nil
+        }
         
         let day = match.isNotEmpty(atRangeIndex: dateNumGroup) ?
             Int(match.string(from: text, atRangeIndex: dateNumGroup))! :
